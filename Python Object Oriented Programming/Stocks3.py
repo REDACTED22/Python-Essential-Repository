fd2c1a7aef71e43cf59ee3a4589b8bb82dad4e88
REDACTED22 <150959413+REDACTED22@users.noreply.github.com>
2024-09-27 23:55:03 +0000

    Reorganized and added files

---
+++ Python Object Oriented Programming/Stocks3.py
@@ -0,0 +1,87 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Programming challenge: add methods for comparison and equality
+
+# Challenge: use a magic method to make stocks and bonds sortable
+# Stocks should sort from low to high on price
+# Bonds should sort from low to high on yield
+
+from abc import ABC, abstractmethod
+
+
+class Asset(ABC):
+    def __init__(self, price):
+        self.price = price
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    def __ge__(self,value):
+        if not isinstance(value,Asset):
+            raise ValueError("Cannot Compare stock to bond!")
+        else:
+            return self.price>=value.price
+    
+    def __lt__(self,value):
+        if not isinstance(value,Asset):
+            raise ValueError("Cannot Compare stock to bond!")
+        else:
+            return self.price<value.price
+        
+    
+
+
+
+class Stock(Asset):
+    def __init__(self, ticker, price, company):
+        super().__init__(price)
+        self.company = company
+        self.ticker = ticker
+    
+  
+    def __str__(self):
+        return(f'{self.ticker}: {self.company} -- ${self.price}')
+
+
+
+
+
+class Bond(Asset):
+    def __init__(self, price, description, duration, yieldamt):
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.yieldamt = yieldamt
+    
+    def __lt__(self, value):
+        if not isinstance(value, Bond):
+            raise ValueError("Cannot compare different asset types!")
+        return self.yieldamt < value.yieldamt
+
+    def __str__(self):
+        return(f'{self.description}: {self.duration} : ${self.price} : {self.yieldamt}%')
+
+
+# ~~~~~~~~~ TEST CODE ~~~~~~~~~
+stocks = [
+    Stock("MSFT", 342.0, "Microsoft Corp"),
+    Stock("GOOG", 135.0, "Google Inc"),
+    Stock("META", 275.0, "Meta Platforms Inc"),
+    Stock("AMZN", 120.0, "Amazon Inc")
+]
+
+bonds = [
+    Bond(95.31, "30 Year US Treasury", 30, 4.38),
+    Bond(96.70, "10 Year US Treasury", 10, 4.28),
+    Bond(98.65, "5 Year US Treasury", 5, 4.43),
+    Bond(99.57, "2 Year US Treasury", 2, 4.98)
+]
+
+stocks.sort()
+bonds.sort()
+
+for stock in stocks:
+    print(stock)
+print("-----------")
+for bond in bonds:
+    print(bond)
---
+++ Python Object Oriented Programming/Stocks4.py
@@ -0,0 +1,72 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Programming challenge: implement a dataclass
+
+# Challenge: convert your classes to dataclasses
+# The subclasses are required to override the magic method
+# that makes them sortable
+
+from abc import ABC
+from dataclasses import dataclass
+
+@dataclass
+class Asset:
+    price:float
+
+    def __ge__(self,value):
+        if not isinstance(value,Asset):
+            raise ValueError("Cannot Compare stock to bond!")
+        else:
+            return self.price>=value.price
+    
+    def __lt__(self,value):
+        if not isinstance(value,Asset):
+            raise ValueError("Cannot Compare stock to bond!")
+        else:
+            return self.price<value.price
+        
+    
+@dataclass
+class Stock(Asset):
+    company:str
+    ticker:str
+    
+
+@dataclass
+class Bond(Asset):
+    description:int
+    duration:int
+    yieldamt:float
+    
+    def __lt__(self, value):
+        if not isinstance(value, Bond):
+            raise ValueError("Cannot compare different asset types!")
+        return self.yieldamt < value.yieldamt
+
+# ~~~~~~~~~ TEST CODE ~~~~~~~~~
+stocks = [
+    Stock("MSFT", 342.0, "Microsoft Corp"),
+    Stock("GOOG", 135.0, "Google Inc"),
+    Stock("META", 275.0, "Meta Platforms Inc"),
+    Stock("AMZN", 120.0, "Amazon Inc")
+]
+
+bonds = [
+    Bond(95.31, "30 Year US Treasury", 30, 4.38),
+    Bond(96.70, "10 Year US Treasury", 10, 4.28),
+    Bond(98.65, "5 Year US Treasury", 5, 4.43),
+    Bond(99.57, "2 Year US Treasury", 2, 4.98)
+]
+
+try:
+   ast = Asset(100.0)
+except:
+   print("Can't instantiate Asset!")
+
+stocks.sort()
+bonds.sort()
+
+for stock in stocks:
+   print(stock)
+print("-----------")
+for bond in bonds:
+   print(bond)
